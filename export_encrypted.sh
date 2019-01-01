d03663feb45df5b460194605653398843bb03da3
Jon Gallagher <jon@nubedehelado.com>
2019-01-01 12:05:07 -0800

    Updates

--- export_encrypted.sh
+++ export_encrypted.sh
@@ -1,3 +1,3 @@
-echo $DATABASE >./DATABASE_file
-echo $USERNAME >./USERNAME_file
-echo $PASSWORD >./PASSWORD_file
+echo { "DATABASE": "$DATABASE"} >./DATABASE_file
+echo { "USERNAME": "$USERNAME"} >./USERNAME_file
+echo { "PASSWORD": "$PASSWORD"} >./PASSWORD_file
